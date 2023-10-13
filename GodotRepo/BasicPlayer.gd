5172750aa5d0d5c30b8a93220b5f5d758ac57dc2
joe-ricker <ee@joericker.net>
2023-10-12 23:20:26 -0500

    Updated motion
    Added vertical parallax
    Added physics and jumping
    Added tilemap

--- GodotRepo/BasicPlayer.gd
+++ GodotRepo/BasicPlayer.gd
@@ -5,7 +5,7 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = 0 # ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
@@ -20,6 +20,7 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
