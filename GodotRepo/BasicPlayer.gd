1478e9b2891e452acd1a9b70fc13a7e19c55f05a
joe-ricker <ee@joericker.net>
2023-10-14 22:08:27 -0500

    State machine player controller
    Upgraded the basic player controller to have a state machine
    Also added animation for player movement

--- GodotRepo/BasicPlayer.gd
+++ GodotRepo/BasicPlayer.gd
@@ -1,29 +1,22 @@
-extends CharacterBody2D
+class_name Player extends CharacterBody2D
 
+@onready
+var state_machine = $"State Machine"
+@onready 
+var animations = $"AnimatedSprite2D"
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+func _ready() -> void:
+	state_machine.init(self)
+	
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+# Pass a reference of the player to the state machine so it can react accordingly
+func _unhandled_input(event: InputEvent) -> void:
+	state_machine.process_input(event)
+	
+func _physics_process(delta: float) -> void:
+	state_machine.process_physics(delta)
+	
+func _process(delta: float) -> void:
+	state_machine.process_frame(delta)
+	
 
-
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
