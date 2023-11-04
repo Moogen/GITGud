7ea281100abd0a0f3e82015e14606c330195ca65
joe-ricker <ee@joericker.net>
2023-11-04 12:36:08 -0500

    Backup

--- GodotRepo/Scripts/GravityWell.gd
+++ GodotRepo/Scripts/GravityWell.gd
@@ -1,17 +1,43 @@
 class_name GravityWell
 extends Node2D
 
-@onready var grav_area : Area2D = $Blackhole
+@onready var grav_area          : Area2D            = $Blackhole
+@onready var grav_center_area   : Area2D            = $Center
+@onready var grav_shape         : CollisionShape2D  = $Blackhole/CollisionShape2D
+@onready var grav_center_shape  : CollisionShape2D  = $Center/CollisionShape2D
+@onready var blackhole_sprite   : Sprite2D          = $BlackholeSprite
+#all the variables for scaling the black hole
+
+const blackhole_gravity : float = 3000
+const blackhole_size    : float = 83
+const center_size       : float = 10
+const click_timer_scale : float = .001 #.1 seconds is = the base size of the black hole
+const sprite_scale      : float = 0.025
+
+var click_time = 0
+    
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+    
+    
+    print("Super massive black hole")
+    grav_area.gravity = blackhole_gravity * (click_timer_scale * click_time)
+    grav_shape.shape.radius = blackhole_size * click_timer_scale * click_time
+    grav_center_shape.shape.radius = center_size * click_timer_scale * click_time
+    blackhole_sprite.scale = Vector2(sprite_scale * click_timer_scale * click_time, sprite_scale * click_timer_scale * click_time)
+    pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+    pass
 
 func remove_gravity():
-	print("testing")
-	grav_area.gravity_space_override = Area2D.SPACE_OVERRIDE_DISABLED
-	pass
+    grav_area.gravity_space_override = Area2D.SPACE_OVERRIDE_DISABLED
+    grav_area.linear_damp_space_override = Area2D.SPACE_OVERRIDE_DISABLED
+    grav_center_area.gravity_space_override = Area2D.SPACE_OVERRIDE_DISABLED
+    grav_center_area.linear_damp_space_override = Area2D.SPACE_OVERRIDE_DISABLED
+    pass
+    
+func set_size(click_time):
+    self.click_time = click_time
+    pass
--- GodotRepo/Scripts/State Machine.gd
+++ GodotRepo/Scripts/State Machine.gd
@@ -11,52 +11,52 @@ var timestop = false
 
 var gravity_influence = Vector2(0,0)
 
-	
+    
 # Initialize the state machine by giving each child state a reference to the
 # parent object it belongs to and enter the default starting_state.
 func init(parent: Player) -> void:
-	for child in get_children():
-		child.parent = parent
+    for child in get_children():
+        child.parent = parent
 
-	# Initialize to the default state
-	change_state(starting_state)
+    # Initialize to the default state
+    change_state(starting_state)
 
 # Change to the new state by first calling any exit logic on the current state.
 func change_state(new_state: State) -> void:
-	if current_state:
-		current_state.exit()
-		
-	previous_state = current_state 
-	current_state = new_state
-	current_state.enter(previous_state)
-	
+    if current_state:
+        current_state.exit()
+        
+    previous_state = current_state 
+    current_state = new_state
+    current_state.enter(previous_state)
+    
 # Pass through functions for the Player to call,
 # handling state changes as needed.
 func process_physics(delta: float) -> void:
-	var new_state = current_state.process_physics(delta, gravity_influence)
-	if new_state:
-		change_state(new_state)
+    var new_state = current_state.process_physics(delta, gravity_influence)
+    if new_state:
+        change_state(new_state)
 
 func process_input(event: InputEvent) -> void:
-	var new_state = current_state.process_input(event)
+    var new_state = current_state.process_input(event)
 
-	if Input.is_action_just_pressed('timestop'): #time slows when pressing F4 as a debug feature
-		timestop = !timestop
-		if(timestop):
-			Engine.set_time_scale(0.1)
-		else:
-			Engine.set_time_scale(1)
-	pass
-	
-	if new_state:
-		change_state(new_state)
+    if Input.is_action_just_pressed('timestop'): #time slows when pressing F4 as a debug feature
+        timestop = !timestop
+        if(timestop):
+            Engine.set_time_scale(0.1)
+        else:
+            Engine.set_time_scale(1)
+    pass
+    
+    if new_state:
+        change_state(new_state)
 
 func process_frame(delta: float) -> void:
-	var new_state = current_state.process_frame(delta)
-	if new_state:
-		change_state(new_state)
+    var new_state = current_state.process_frame(delta)
+    if new_state:
+        change_state(new_state)
 
 func set_gravity(influence: Vector2) -> void:
-	gravity_influence = influence
-	get_tree().call_group("Debug Group", "update_gravity_influence", gravity_influence)
-	pass
+    gravity_influence = influence
+    get_tree().call_group("Debug Group", "update_gravity_influence", gravity_influence)
+    pass
