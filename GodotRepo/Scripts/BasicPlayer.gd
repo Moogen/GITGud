0e3d75b0f5ce7f71853ae5e3495d7126ff2ecb18
AvacadJoe <123843754+AvacadJoe@users.noreply.github.com>
2023-11-04 14:10:28 -0500

    Blackhole Size + Overlap Fix (#27)

--- GodotRepo/Scripts/BasicPlayer.gd
+++ GodotRepo/Scripts/BasicPlayer.gd
@@ -8,16 +8,24 @@ var animations = $"AnimatedSprite2D"
 
 #implement some signal functions to pass player data to GUI
 func _ready() -> void:
-	state_machine.init(self)
+    state_machine.init(self)
 
 # Pass a reference of the player to the state machine so it can react accordingly
 func _unhandled_input(event: InputEvent) -> void:
-	state_machine.process_input(event)
-	
+    state_machine.process_input(event)
+    
 func _physics_process(delta: float) -> void:
-	state_machine.process_physics(delta)
-	
+    state_machine.process_physics(delta)
+    
 func _process(delta: float) -> void:
-	state_machine.process_frame(delta)
+    state_machine.process_frame(delta)
+    
+func _add_gravity():
+    print("Adding gravity")
+    pass
+    
+func _remove_gravity():
+    print("Removing gravity")
+    pass
 
 
