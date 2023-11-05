34d60a9d895e35c94690a4b6f25f1160eadff279
joe-ricker <ee@joericker.net>
2023-11-05 15:14:33 -0600

    Backup

--- GodotRepo/Scripts/GravityWell.gd
+++ GodotRepo/Scripts/GravityWell.gd
@@ -3,13 +3,17 @@ extends Node2D
 
 @onready var grav_area          : Area2D            = $Blackhole
 @onready var grav_center_area   : Area2D            = $Center
+@onready var horizon_area       : Area2D            = $EventHorizon
 @onready var grav_shape         : CollisionShape2D  = $Blackhole/CollisionShape2D
 @onready var grav_center_shape  : CollisionShape2D  = $Center/CollisionShape2D
+@onready var horizon_shape      : CollisionShape2D  = $EventHorizon/CollisionShape2D
 @onready var blackhole_sprite   : Sprite2D          = $BlackholeSprite
 #all the variables for scaling the black hole
 
 const blackhole_gravity : float = 3000
+const horizon_gravity   : float = 3000*10
 const blackhole_size    : float = 83*5
+const horizon_size      : float = 83
 const center_size       : float = 10
 const click_timer_scale : float = .001 #.1 seconds is = the base size of the black hole
 const sprite_scale      : float = 0.025
@@ -21,7 +25,9 @@ var mass_cost = 0
 func _ready():
     print("Super massive black hole")
     grav_area.gravity = blackhole_gravity * (click_timer_scale * click_time)
+    horizon_area.gravity = horizon_gravity * (click_timer_scale * click_time)
     grav_shape.shape.radius = blackhole_size * click_timer_scale * click_time
+    horizon_shape.shape.radius = horizon_size * click_timer_scale * click_time
     grav_center_shape.shape.radius = center_size * click_timer_scale * click_time
     blackhole_sprite.scale = Vector2(sprite_scale * click_timer_scale * click_time, sprite_scale * click_timer_scale * click_time)
     
@@ -35,11 +41,14 @@ func _process(delta):
     for body in grav_area.get_overlapping_bodies():
         if body is Player:
             body.set_influence(grav_area.gravity, self.global_position)
+            
     pass
 
 func remove_gravity():
     grav_area.gravity_space_override = Area2D.SPACE_OVERRIDE_DISABLED
     grav_area.linear_damp_space_override = Area2D.SPACE_OVERRIDE_DISABLED
+    horizon_area.gravity_space_override = Area2D.SPACE_OVERRIDE_DISABLED
+    horizon_area.linear_damp_space_override = Area2D.SPACE_OVERRIDE_DISABLED
     grav_center_area.gravity_space_override = Area2D.SPACE_OVERRIDE_DISABLED
     grav_center_area.linear_damp_space_override = Area2D.SPACE_OVERRIDE_DISABLED
     
